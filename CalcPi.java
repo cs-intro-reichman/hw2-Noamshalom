0924718e9bacace079f746e079b59458411fb174
Noam Shalom <noam.shalom@post.runi.ac.il>
2025-11-12 01:37:59 +0200

    completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,22 +1,28 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    int terms = Integer.parseInt(args[0]);
-		Double sum = 1.0;
-		Double even = 3.0;
-		Double prime = 5.0;
-		for(int i=2; i<=terms; i++){
-			if (i%2==0) {
-				sum = sum - (1/even);
-				even = even + 4;
-			} else{
-				sum = sum + (1/prime);
-				prime = prime + 4;
-			}
+    public static void main(String[] args) {
+        int terms = Integer.parseInt(args[0]);
 
-		}
-		double result = sum*4;
-		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + result);
-	}
+        double sum = 1.0;
+        double odd = 3.0;      
+        for (int i = 2; i <= terms; i++) {
+            if (i % 2 == 0) {
+                sum -= 1.0 / odd;
+            } else {
+                sum += 1.0 / odd;
+            }
+            odd += 2.0;
+        }
+        double result = sum * 4.0;
+
+      
+        String sPi = Double.toString(Math.PI);
+        System.out.println("pi according to Java: " + sPi);
+
+     
+        String sApprox = Double.toString(result);
+        int pad = Math.max(0, sPi.length() - sApprox.length());
+        StringBuilder spaces = new StringBuilder();
+        for (int i = 0; i < pad; i++) spaces.append(' ');
+        System.out.println("pi, approximated: " + spaces + sApprox);
+    }
 }
