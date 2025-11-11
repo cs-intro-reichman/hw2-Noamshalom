5fce810f6ba53a67ddd9397dc2e57b9981219d25
Noam Shalom <noam.shalom@post.runi.ac.il>
2025-11-12 01:47:45 +0200

    completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -3,26 +3,15 @@ public class CalcPi {
         int terms = Integer.parseInt(args[0]);
 
         double sum = 1.0;
-        double odd = 3.0;      
+        double denom = 3.0;       
         for (int i = 2; i <= terms; i++) {
-            if (i % 2 == 0) {
-                sum -= 1.0 / odd;
-            } else {
-                sum += 1.0 / odd;
-            }
-            odd += 2.0;
+            if (i % 2 == 0) sum -= 1.0 / denom; 
+            else            sum += 1.0 / denom; 
+            denom += 2.0;
         }
-        double result = sum * 4.0;
+        double approx = sum * 4.0;
 
-      
-        String sPi = Double.toString(Math.PI);
-        System.out.println("pi according to Java: " + sPi);
-
-     
-        String sApprox = Double.toString(result);
-        int pad = Math.max(0, sPi.length() - sApprox.length());
-        StringBuilder spaces = new StringBuilder();
-        for (int i = 0; i < pad; i++) spaces.append(' ');
-        System.out.println("pi, approximated: " + spaces + sApprox);
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated: " + approx);
     }
 }
