d850b92bfa264f16961e7ea71800f7262d7b7514
Noam Shalom <noam.shalom@post.runi.ac.il>
2025-11-12 01:28:36 +0200

    completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,20 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	       String word = (args[0]);
-               int number = Integer.parseInt(args[1]);
-               word = word.toUpperCase();
+    public static void main(String[] args) {
+        String word = args[0].toUpperCase();
+        int times = Integer.parseInt(args[1]);
 
-               for (int i = 0; i<(word.length()); i++) {
-                if("AEFHILMNORSX".indexOf(word.charAt(i))!= -1){
-                        System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
-                } else {
-                        System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
-                }
-               }
-               System.out.println("What does that spell? ");
-               for(int i=0; i<number; i++){
-                System.out.println(word + "!!!");
-               }
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (usesAn(ch)) {
+                System.out.println("Give me an " + ch + ": " + ch + "!");
+            } else {
+                System.out.println("Give me a "  + ch + ": " + ch + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+        for (int i = 0; i < times; i++) {
+            System.out.println(word + "!!!");
+        }
+    }
+
+    // Letters that take "an" per the assignment: A,E,F,H,I,L,M,N,O,R,S,X
+    private static boolean usesAn(char c) {
+        return "AEFHILMNORSX".indexOf(c) >= 0;
+    }
 }
