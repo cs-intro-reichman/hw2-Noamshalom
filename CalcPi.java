80895c576990a70b736e9bb49dbe5fdbbfd2a2ff
Noam Shalom <noam.shalom@post.runi.ac.il>
2025-11-12 01:13:44 +0200

    completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int terms = Integer.parseInt(args[0]);
+		Double sum = 1.0;
+		Double even = 3.0;
+		Double prime = 5.0;
+		for(int i=2; i<=terms; i++){
+			if (i%2==0) {
+				sum = sum - (1/even);
+				even = even + 4;
+			} else{
+				sum = sum + (1/prime);
+				prime = prime + 4;
+			}
+
+		}
+		double result = sum*4;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + result);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,38 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) {
+
+        int N = Integer.parseInt(args[0]);
+        String mood = args[1];
+
+        if (mood.equals("v")) {
+
+            int numRows = N;
+
+            for (int i = 1; i <= numRows; i++) {
+
+                int num = i;
+                System.out.print(num + " ");
+                int count = i;
+
+                while (num != 1 || count == 1) {
+                    if (num % 2 == 0){
+                        num = num / 2;
+                    } else{
+                        num = num * 3 + 1;
+                    }
+                    count ++;
+                    System.out.print(num + " ");
+                }
+                
+                System.out.println("(" + count + ")");
+            }
+
+            System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+        }
+        
+        else{
+            System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+        }
+    }
 }
+                    
