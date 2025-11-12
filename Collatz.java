9f8f17a3c09761c424d61aa4a3c46c852de0d546
Noam Shalom <noam.shalom@post.runi.ac.il>
2025-11-12 11:34:40 +0200

    completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,38 +1,31 @@
 public class Collatz {
     public static void main(String[] args) {
-
         int N = Integer.parseInt(args[0]);
-        String mood = args[1];
+        String mode = args[1];
 
-        if (mood.equals("v")) {
-
-            int numRows = N;
-
-            for (int i = 1; i <= numRows; i++) {
-
-                int num = i;
+        if (mode.equals("v")) {
+            for (int seed = 1; seed <= N; seed++) {
+                int num = seed;
+                int count = 1;            
                 System.out.print(num + " ");
-                int count = i;
 
+                
                 while (num != 1 || count == 1) {
-                    if (num % 2 == 0){
+                    if (num % 2 == 0) {
                         num = num / 2;
-                    } else{
+                    } else {
                         num = num * 3 + 1;
                     }
-                    count ++;
+                    count++;               
                     System.out.print(num + " ");
                 }
-                
+
                 System.out.println("(" + count + ")");
             }
 
             System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-        }
-        
-        else{
+        } else { // mode "c"
             System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
         }
     }
 }
-                    
